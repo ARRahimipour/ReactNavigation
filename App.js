4bd6135459b1775840528b811dd959b9682a79ce
ARRahimipour <arrahimipouranaraki@gmail.com>
2024-07-31 16:49:07 +0330

    Set Navigation With Button

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
+import DetailsScreen from './components/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
